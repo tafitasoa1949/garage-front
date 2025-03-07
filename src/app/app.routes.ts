3eb2ebc7faa8df4d7140c9e9cf8941c39ea188b1
Tafitasoa RATSIMBAHARISON <tafita.r@MDGIT.LOCAL>
2025-03-07 15:41:15 +0300

    code de base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/components/not-found/not-found.component';
+import { TestComponent } from './test/components/test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'test', component: TestComponent },
+    { path: '', redirectTo: 'test', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent }
+
+];
