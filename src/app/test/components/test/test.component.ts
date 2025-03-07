6ce099b3029684fba7ad3ddd55fd58dfa7310078
Tafitasoa RATSIMBAHARISON <tafita.r@MDGIT.LOCAL>
2025-03-07 16:08:12 +0300

    install primeng/primeicon/primeng

--- src/app/test/components/test/test.component.ts
+++ src/app/test/components/test/test.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
